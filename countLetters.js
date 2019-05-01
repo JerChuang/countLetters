6c4761aa933bc821edad857add8443adf03e67ce
Jerry <chih.chia.chuang@gmail.com>
2019-05-01 19:56:14 +0000

    update code to accept input from console

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,27 @@
+var words = process.argv.slice(2).join("");
+
 function countLetters(input){
   var output ={};
   var keys = [];
-
   //change input into no space lower case
   var string = input.toLowerCase().split(" ").join("");
- // looping through input string
+  // looping through input string
   for (let i of string){
     keys = Object.keys(output);
     var exists = false;
     //checking if letter already exists
-    //looping through the keys of object
+    //looping through the keys of output
     for(let j of keys){
       if (i === j) {
         output[j] +=1;
         exists = true;
       }
-
     }
-    if (exists == false){
-
+    //if letter does not exist in output, add to keys
+    if (exists === false){
         output[i] = 1;
-
       }
   }
   return output;
 }
-console.log (countLetters("Lighthouse in the house"));
+console.log (countLetters(words));
